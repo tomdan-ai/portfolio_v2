474ee375e6c2c200a17e5a08f3f87262c4fed745
tomdan-ai <tomudoh258@gmail.com>
2024-01-29 20:44:56 +0100

    Begin development

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,19 +5,19 @@ import Link from "next/link";
 const Header: React.FC = () => {
   return (
     <>
-      <header className="bg-blue-500">
-        <nav>
-          <div>
+      <header className="bg-blue w-80 h-24">
+                <nav className="flex gap-x-2.5 border-b-4 border-indigo-500">
+          <div className="border-solid border-2 border-sky-500">
             <p>Tom Udoh</p>
           </div>
           <ul className="flex space-x-4">
             <li className="text-red">
               <Link href="/">Home</Link>
             </li>
-            <li className="text-white">
+            <li className="text-black">
               <Link href="/about">About</Link>
             </li>
-            <li className="text-white">
+            <li className="text-black">
               <Link href="/projects">Projects</Link>
             </li>
           </ul>
